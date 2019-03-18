3151af6660e5673a931485711763b2b4e849bdf5
wangyanhua <332468230@qq.com>
2019-03-18 16:42:56 +0800

    初始代码内容 提交

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**
+ * 
+ * ClassName: DemoApplication <br/>
+ * Function:<br/>
+ * date: 2019年3月18日 下午2:59:03 <br/>
+ *
+ * @author Yan
+ * @version
+ * @since JDK 1.6
+ */
+public class DemoApplication {
+
+    /**
+     * 
+     * main: <br/>
+     *
+     * @author Yan
+     * @param args
+     * @since JDK 1.6
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    /**
+     * 
+     * showMainInfomesion: <br/>
+     *
+     * @author Yan
+     * @since JDK 1.6
+     */
+    @SuppressWarnings("unused")
+    private void showMainInfomesion() {
+
+    }
+}
